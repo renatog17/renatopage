ee21242ae778c0c1719f911167433e979fac18d0
Renato Oliveira <renatog.oliveira17@gmail.com>
2025-05-19 01:01:27 -0300

    reset css

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Header from "./components/Header/Header";
 import Projetos from "./components/Projetos/Projetos";
 import Hero from "./components/Hero/Hero";
 
+
+
 function App() {
   return (
     <div>
       <Header></Header>
       <Hero></Hero>
-      
+      <Projetos></Projetos>
     </div>
   );
 }
--- src/components/Projetos/Projetos.jsx
+++ src/components/Projetos/Projetos.jsx
@@ -1,17 +1,30 @@
+import "./Projetos.css"
+
 export default function Projetos() {
   const projetos = [
     {
       nome: "Kit Card",
       descricao: "Aplicativo de flash cards para aprender idiomas.",
-      link: "https://github.com/seu-usuario/kit-card",
+      link: "https://github.com/renatog17/flashcards-react",
     },
   ];
 
   return( 
-      <div>Meus Projetos Pessoais
-        <div>{projetos.map((projeto ,index) => (
-            <div key={index} >{projeto.nome}</div>
-        ))}</div>
-      </div>
+    <div className="container">
+    <h2 className="titulo">Meus Projetos Pessoais</h2>
+    <div className="grid-projetos">
+      {projetos.map((projeto, index) => (
+        <div className="card" key={index}>
+          <h3>{projeto.nome}</h3>
+          {projeto.descricao && <p>{projeto.descricao}</p>}
+          {projeto.link && (
+            <a href={projeto.link} target="_blank" rel="noopener noreferrer">
+              Ver projeto
+            </a>
+          )}
+        </div>
+      ))}
+    </div>
+  </div>
 )
 }
